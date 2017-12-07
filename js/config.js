c08d5e634cfdaba309fcf74e03bec320334ede86
Ryan Witko <witko.ryan@gmail.com>
2017-12-06 22:16:34 -0500

    Cloning AWS WildRydes app as starting point.

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_Rcp9PaeBF', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5umn9glkbjchf9heg8a1v79in2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://c9dsswmo0f.execute-api.us-east-1.amazonaws.com/Production' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
